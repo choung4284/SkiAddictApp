0485a4bf863d6ff485e31d88c3c491cbd14f42fb
choung4284 <choung4284@gmail.com>
2026-09-22 01:04:53 +0700

    Use real HOME drawable resource

--- app/src/main/java/fun/skiaddict/projector/MainActivity.java
+++ app/src/main/java/fun/skiaddict/projector/MainActivity.java
@@ -1,23 +1,18 @@
 package fun.skiaddict.projector;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class MainActivity extends Activity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
@@ -29,46 +24,20 @@ public class MainActivity extends Activity {
         );
 
         FrameLayout root = new FrameLayout(this);
-        root.setBackgroundColor(Color.rgb(239, 246, 250));
+        root.setBackgroundColor(Color.rgb(240, 247, 252));
 
-        ImageView image = new ImageView(this);
-        image.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        image.setAdjustViewBounds(false);
+        ImageView master = new ImageView(this);
+        master.setImageResource(R.drawable.home_master_reference);
+        master.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        master.setAdjustViewBounds(false);
+        master.setBackgroundColor(Color.rgb(240, 247, 252));
 
-        Bitmap bitmap = loadMaster();
-        if (bitmap != null) image.setImageBitmap(bitmap);
-
-        root.addView(image, new FrameLayout.LayoutParams(
+        root.addView(master, new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT,
                 FrameLayout.LayoutParams.MATCH_PARENT,
                 Gravity.CENTER
         ));
 
         setContentView(root);
-    }
-
-    private Bitmap loadMaster() {
-        int[] ids = {
-                R.raw.home_master_0,
-                R.raw.home_master_1,
-                R.raw.home_master_2,
-                R.raw.home_master_3,
-                R.raw.home_master_4,
-                R.raw.home_master_5
-        };
-        StringBuilder b64 = new StringBuilder(100000);
-        try {
-            for (int id : ids) {
-                InputStream in = getResources().openRawResource(id);
-                BufferedReader br = new BufferedReader(new InputStreamReader(in));
-                String line;
-                while ((line = br.readLine()) != null) b64.append(line.trim());
-                br.close();
-            }
-            byte[] data = Base64.decode(b64.toString(), Base64.DEFAULT);
-            return BitmapFactory.decodeByteArray(data, 0, data.length);
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
